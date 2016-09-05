4e07232ad467247cfc7e4f4aa840f751253af6e0
Damian Sosnowski <sosnowski@outlook.com>
2016-09-05 23:44:15 +0200

    Inital commit with signal generator

---
+++ FSharpMusic/Program.fs
@@ -0,0 +1,19 @@
+﻿
+// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+open System.IO
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+
+    let write fileName (ms: MemoryStream) =
+        use fs = new FileStream(Path.Combine(".", fileName), FileMode.Create)
+        ms.WriteTo(fs)
+
+    Array.ofSeq (SignalGenerator.generateSamples 5000.0 440.0)
+    |> WavePacker.pack
+    |> write "test.wav"
+
+    0 // return an integer exit code
---
+++ FSharpMusic/SignalGenerator.fs
@@ -0,0 +1,18 @@
+﻿module SignalGenerator
+
+let generateSamples miliseconds frequency = 
+    let sampleRate = 44100.0
+    let sizteenBitSampleLimit = 32767.0
+    let volume = 0.8
+    let toAmplitude x = 
+        x
+        |> (*) (2.0 * System.Math.PI * frequency / sampleRate)
+        |> sin
+        |> (*) sizteenBitSampleLimit
+        |> (*) volume
+        |> int16
+    
+    let numOfSamples = miliseconds / 1000.0 * sampleRate
+    let requiredSamples = seq { 1.0..numOfSamples }
+
+    Seq.map toAmplitude requiredSamples
